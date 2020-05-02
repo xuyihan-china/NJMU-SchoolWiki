bf1086ade26e91a5aebb14445d55ec0910f3251b
xuyihan-china <848247256@qq.com>
2020-05-02 12:18:42 +0800

    00

--- zoomrepeat/curd/app.js
+++ zoomrepeat/curd/app.js
@@ -1,25 +1,14 @@
 var express = require('express');
-var fs = require('fs');
+var router = require('./router');
+var bodyParser = require('body-parser')
+//var fs = require('fs');
 var app = express();
 app.use('/node_modules/',express.static('./node_modules/'));
 app.use('/public/',express.static('./public/'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.engine('html',require('express-art-template'));
-app.get('/',function(req,res){
-    fs.readFile('./db.json','utf-8',function(err,data){
-        if(err) {
-            return res.status(500).send('server error');
-        }
-        res.render('index.html',{
-            fruits:[
-                'apple',
-                'banana',
-                'orange'
-            ],
-            students:JSON.parse(data).students
-        });
-    })
-   
-})
+app.use(router);
 app.listen(3000,function(req,res){
     console.log('sever is running at 3000');
 })
---
+++ zoomrepeat/curd/router.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var Student = require('./student-fs');
+var express = require('express');
+var router = express.Router();
+//创建一个路由容器
+//把方法都挂在router中
+router.get('/students', function (req, res) {
+    fs.readFile('./db.json', 'utf-8', function (err, data) {
+        if (err) {
+            return res.status(500).send('server error');
+        }
+        res.render('index.html', {
+            fruits: [
+                'apple',
+                'banana',
+                'orange'
+            ],
+            students: JSON.parse(data).students
+        });
+    })
+})
+router.get('/students/new', function (req, res) {
+    res.render('new.html');
+})
+router.post('/students/new', function (req, res) {
+    //JSON.parse(req.body);
+    Student.save(req.body, function (err) {
+        if (err) {
+            return res.status(500).send('server not found')
+        }
+        res.redirect('/students')
+    })
+})
+router.get('/students/edit', function (req, res) {
+    Student.findById(parseInt(req.query.id), function (err, student) {
+        if (err) {
+          return res.status(500).send('Server error.')
+        }
+        res.render('edit.html', {
+          student: student
+        })
+      })
+})
+router.post('/students/edit', function (req, res) {
+    Student.updateById(req.body, function (err) {
+        if (err) {
+          return res.status(500).send('Server error.')
+        }
+        res.redirect('/students')
+      })
+})
+router.get('/students/delete', function (req, res) {
+    Student.deleteById(req.query.id, function (err) {
+        if (err) {
+          return res.status(500).send('Server error.')
+        }
+        res.redirect('/students')
+        //要跳转的界面 
+    })
+})
+//把router导出
+module.exports = router;
