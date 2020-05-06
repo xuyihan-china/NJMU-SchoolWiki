17890a8cc3aab31ead4ffa9632754e5ea42d9358
xuyihan-china <848247256@qq.com>
2020-05-06 11:13:49 +0800

    00

--- zoomrepeat/app2.js
+++ zoomrepeat/app2.js
@@ -3,6 +3,7 @@ var http = require('http');
 var fs =  require('fs');
 //如果有外链标签 浏览器会对这些资源请求 link script img 
 //为了统一处理静态资源 要把所有的静态资源都放到public目录中
+// data 是二进制数据 data.toString 是如果你要操作 模板渲染 如果直接在网上渲染 不需要
 http
     .createServer(function(req,res){
         var url = req.url;
