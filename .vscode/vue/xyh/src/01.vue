24785509795a4f73a5780ebe053af08a0eb1243a
xuyihan-china <848247256@qq.com>
2020-05-08 20:00:27 +0800

    00

---
+++ .vscode/vue/xyh/src/01.vue
@@ -0,0 +1,22 @@
+<div id ="vue">
+  <h1>
+    site:{{site}};
+    url:{{url}};
+    {{details()}};
+  </h1>
+</div>
+<script>
+    var vm = new Vue({
+        el:'#vuedet',
+        data:{
+            site:"cn",
+            url:"www",
+            alexa:"10000"
+        },
+        methods:{
+            details:function(){
+                return this.site+"dream";
+            }
+        }
+    })
+</script>
