be3120c6d0d9856463ad2293e6df19b3184d7d50
xuyihan-china <848247256@qq.com>
2020-06-14 20:28:11 +0800

    00

--- .vscode/mall/src/App.vue
+++ .vscode/mall/src/App.vue
@@ -1,14 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2020-06-01 22:52:24
- * @LastEditTime: 2020-06-01 23:25:58
+ * @LastEditTime: 2020-06-13 20:47:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \test\.vscode\mall\src\App.vue
 --> 
 <template>
   <div id="app">
-   
   </div>
 </template>
 
