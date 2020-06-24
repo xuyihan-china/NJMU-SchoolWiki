996f0fa3e51b71205a341e2a46318010d843c82e
xuyihan-china <848247256@qq.com>
2020-06-24 22:25:43 +0800

    00

--- .vscode/01.js
+++
@@ -1,5 +0,0 @@
-let str1 ='jspang.com';
-
-xiedajiao = str1 => {
-    return `hi${str1}`;
-};
---
+++ .vscode/supermall/src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-24 22:09:49
+ * @LastEditTime: 2020-06-24 22:12:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \.vscode\supermall\src\App.vue
+--> 
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
