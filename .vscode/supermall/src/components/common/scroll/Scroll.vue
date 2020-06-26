e512312c7fb97a136d066941d6d42be10e8093ec
xuyihan-china <848247256@qq.com>
2020-06-26 22:02:53 +0800

    00

---
+++ .vscode/supermall/src/components/common/scroll/Scroll.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-26 21:48:09
+ * @LastEditTime: 2020-06-26 21:59:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \.vscode\supermall\src\components\common\scroll\Scroll.vue
+--> 
+<template>
+  <div>
+      <!-- ref  -->
+      <div class="wrapper" ref="">
+          <div class="content">
+              <slot></slot>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+    name:"Scroll",
+    date(){
+        return {
+            scroll:null
+        }
+    },
+    mounted(){
+        this.scroll = new Bscroll('.wrapper',{
+
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
--- .vscode/supermall/src/views/home/Home.vue
+++ .vscode/supermall/src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-06-24 23:33:02
- * @LastEditTime: 2020-06-26 18:03:36
+ * @LastEditTime: 2020-06-26 21:50:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \.vscode\supermall\src\views\home\Home.vue
@@ -11,13 +11,18 @@
     <nav-bar id="yyc">
       <div slot="center">京东少年家</div>
     </nav-bar>
-    <home-swiper :banners="banners" id="home-swiper"></home-swiper>
-    <!-- home 大组件中 保留最少的标签值 -->
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control :title="['流行','新款','精选']" id="tac" 
-                  @tabClick="tabClick"></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
+    <!--< div class="wrapper">
+      <div class="content"> -->
+        <scroll class="content"><!-- 要滚动的内容 -->
+        <home-swiper :banners="banners" id="home-swiper"></home-swiper>
+        <!-- home 大组件中 保留最少的标签值 -->
+        <recommend-view :recommends="recommends"></recommend-view>
+        <feature-view></feature-view>
+        <tab-control :title="['流行','新款','精选']" id="tac" @tabClick="tabClick"></tab-control>
+        <goods-list :goods="showGoods"></goods-list>
+        </scroll>
+      <!-- </div>
+    </div> -->
   </div>
 </template>
 
@@ -28,9 +33,10 @@ import featureView from "views/home/childComps/featureView";
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
-import GoodsList from "components/content/goods/GoodsList"
+import GoodsList from "components/content/goods/GoodsList";
 //网络请求数据
-import { getHomeMultidata , getHomeGoods} from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import Scroll from "components/common/scroll/Scroll"
 export default {
   name: "Home",
   components: {
@@ -39,87 +45,87 @@ export default {
     RecommendView,
     featureView,
     TabControl,
-    GoodsList
+    GoodsList,
+    Scroll
   },
-  computed:{
-    showGoods(){
-      return this.goods[this.currentType].list
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     }
   },
   data() {
     return {
       banners: [],
       recommends: [],
-      goods:{
-        'pop':{page:0,list:[]},
-        'new':{page:0,list:[]},
-        'sell':{page:0,list:[]}
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
       },
-      currentType:'pop'
-    }
+      currentType: "pop"
+    };
   },
   created() {
     //请求多个数据
-    this.getHomeMultidata()
+    this.getHomeMultidata();
     //2.请求商品数据
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
-  methods:{
+  methods: {
     //自己的方法 事件监听相关的方法
-    tabClick(index){
-      switch(index){
+    tabClick(index) {
+      switch (index) {
         case 0:
-          this.currentType='pop'
-          break
+          this.currentType = "pop";
+          break;
         case 1:
-          this.currentType='new'
-          break
+          this.currentType = "new";
+          break;
         case 2:
-          this.currentType='sell'
-          break
+          this.currentType = "sell";
+          break;
       }
     },
     //网络请求
-    getHomeMultidata(){
+    getHomeMultidata() {
       getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-      })
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
     //上面调用了三次这种方法 每一次传递不通的参数 加载首页的时候就加载好了
-    getHomeGoods(type){
-    const page = this.goods[type].page+1
-    getHomeGoods(type,page).then(res=>{
-      //console.log(res.data.list);
-      this.goods[type].list.push(...res.data.list)
-      this.goods[type].page +=1
-    })
-    }    
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        //console.log(res.data.list);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    }
   }
 };
 </script>
 
 <style scoped>
-
 #yyc {
-  position:fixed;
-  right:0;
+  position: fixed;
+  right: 0;
   left: 0;
   top: 0;
   background-color: rgb(226, 49, 49);
   color: white;
   z-index: 9;
 }
-.tab-control{
-  position:sticky;
+.tab-control {
+  position: sticky;
   top: 44px;
 }
-#home-swiper{
+#home-swiper {
   padding-top: 44px;
 }
-#tac{
+#tac {
   z-index: 100;
 }
 </style>
