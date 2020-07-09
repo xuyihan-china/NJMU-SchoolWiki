e203151c44ae330721405004e1a473d4f511f387
xuyihan-china <848247256@qq.com>
2020-07-09 17:33:41 +0800

    00

--- .vscode/supermall/src/App.vue
+++ .vscode/supermall/src/App.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: your name
  * @Date: 2020-06-24 22:09:49
- * @LastEditTime: 2020-06-29 16:46:47
+ * @LastEditTime: 2020-07-09 17:32:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \.vscode\supermall\src\App.vue
+ * @FilePath: \.vscode\supermall\src\App.
 --> 
 <template>
   <div id="app">
